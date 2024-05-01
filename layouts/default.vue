c0ab8cd449d212d851929fc95621784cb322a2dc
Андрей Рыжинский <86745524+andreykaaaaa@users.noreply.github.com>
2024-05-01 16:56:17 +0300

    feat: added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="default">
+    <div class="body">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+.default {
+  background-color: rgb(247 254 231);
+  height: max-content;
+  min-height: 100%;
+}
+.body {
+  width: 80%;
+  min-height: 100%;
+  margin: 0 auto;
+  background-color: #fff;
+  border-radius: 20px;
+  height: max-content;
+}
+</style>
